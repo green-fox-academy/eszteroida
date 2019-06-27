f7e75f69cd60e3c912e338247ed44e6063de529a
eszteroida <eszteroida@eszteroidas-MacBook-Air.local>
2019-06-27 17:24:28 +0200

    end of day04

---
+++ week-01/day04/conditional-variable-mutation.ts
@@ -0,0 +1,40 @@
+let a: number = 24;
+let out: number = 0;
+
+// if a is even increment out by one
+
+console.log(out);
+
+let b: number = 13;
+let out2: string = '';
+
+// if b is between 10 and 20 set out2 to "Sweet!"
+// if less than 10 set out2 to "Less!",
+// if more than 20 set out2 to "More!"
+
+console.log(out2);
+
+let c: number = 123;
+let credits: number = 100;
+let isBonus: boolean = false;
+
+// if credits are at least 50,
+// and is_bonus is false decrement c by 2
+// if credits are smaller than 50,
+// and is_bonus is false decrement c by 1
+// if is_bonus is true c should remain the same
+
+console.log(c);
+
+let d: number =  8;
+let time: number = 120;
+let out3: string = '';
+
+// if d is dividable by 4
+// and time is not more than 200
+// set out3 to "check"
+// if time is more than 200
+// set out3 to "Time out"
+// otherwise set out3 to "Run Forest Run!"
+
+console.log(out3);
--- week-01/day04/seconds-in-a-day.ts
+++ week-01/day04/seconds-in-a-day.ts
@@ -1,6 +1,11 @@
 let currentHours: number = 14;
 let currentMinutes: number = 34;
 let currentSeconds: number = 42;
+let secondsPast: number = currentSeconds + (60 * currentMinutes) + (60 * 60 * currentHours);
+let secondsLeft: number = ((24 * 60 * 60) - secondsPast);
+
+console.log('There are ' + secondsLeft + ' seconds left till midnight.');
+
 
 // Write a program that prints the remaining seconds (as an integer) from a
 // day if the current time is represented by these variables
