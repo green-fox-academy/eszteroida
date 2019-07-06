007b8788dae89e9c167be2c1a2f70fc8eaa5aeae
eszteroida <eszteroida@eszteroidas-MacBook-Air.local>
2019-07-06 11:32:23 +0200

    parametric_av

---
+++ week-01/day05/draw-pyramid.ts
@@ -0,0 +1,28 @@
+'use strict';
+
+let lineCount: number = 4;
+
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
+
+let starsToDraw: string = "";
+let spacesToDraw: string = ""
+
+for (let i: 1; i <= lineCount; i++) {
+    for (let s: number 1; s <= lineCount; s++){
+    
+        
+    
+
+    }
+
+    //console.log(starsToDraw);
+
+}
--- week-01/day05/parametric-average.ts
+++ week-01/day05/parametric-average.ts
@@ -3,8 +3,14 @@
 // Write a program that calculates the sum and the average from 1 to a given number.
 // Example input: 5
 // Example output: Sum: 15, Average: 3
+let inputNumber: number = 8;
+let sum: number = 0;
 
 
-for (let first: number = 1; first < )
+for (let i: number = 1; i <= inputNumber; i++){
+sum += i;
 
-console.log('Sum: ' + brrrrr + ', Average: ' + kéalsfdkj);
+}
+
+console.log('Sum: ' + sum + ', Average: ' + sum / inputNumber);
+
