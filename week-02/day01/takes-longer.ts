814eb645c79b967a7d31b1c788ee3dc137222c2f
eszteroida <eszteroida@eszteroidas-MacBook-Air.local>
2019-07-06 22:13:45 +0200

    takeslonger

---
+++ week-02/day01/takes-longer.ts
@@ -0,0 +1,10 @@
+'use strict';
+// While saving this quote, a disk error has occured. Please fix it.
+// Add "always takes longer than" between the words "It" and "you"
+
+let quote: string = `Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.`;
+
+// egyik megoldás, de nem ez a legjobb
+//quote = quote.replace('It', 'It always takes longer than');
+
+console.log(quote);
