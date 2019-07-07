133ec0f2a4afe35df01954a33b80b90e3ee85eb3
eszteroida <eszteroida@eszteroidas-MacBook-Air.local>
2019-07-07 15:58:46 +0200

    personal finance

--- week-02/day02/Personal-finance.ts
+++ week-02/day02/Personal-finance.ts
@@ -3,17 +3,21 @@
 let moneySpent: number[] = [500, 1000, 1250, 175, 800, 120];
 
 //How much did we spend?
-
-
-console.log(`We spent ${}`);
+let sum: number = 0;
+for (let i: number = 0; i <= moneySpent.length - 1; i++) {
+    sum += moneySpent[i];
+}
+console.log(sum);
 
 //Which was our greatest expense?
-
+let greatestExpense: number = Math.max(...moneySpent);
 console.log(greatestExpense);
 
 //Which was our cheapest spending?
-
+let cheapestExpense: number = Math.min(...moneySpent);
 console.log(cheapestExpense);
+
 //What was the average amount of our spendings?
+let avrgAmount: number = sum / (moneySpent.length);
 
 console.log(avrgAmount);
