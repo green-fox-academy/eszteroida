40957f49cbdc7ea5c9992dbc41722c136ad18061
eszteroida <eszteroida@eszteroidas-MacBook-Air.local>
2019-07-09 12:15:40 +0200

    w03d02-print-each

---
+++ week-03/day02/count-lines.ts
@@ -0,0 +1,18 @@
+'use strict';
+declare function require(path: string): any;
+export {};
+
+const fs = require('fs');
+
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+let fileName : string = '';
+function numberOfLines () {
+
+
+}
+
+console.log();
---
+++ week-03/day02/print-each-line.ts
@@ -0,0 +1,18 @@
+'use strict';
+declare function require(path: string): any;
+export {};
+
+const fs = require('fs');
+// Write a program that opens a file called "my-file.txt", then prints
+// each line from the file.
+// If the program is unable to read the file (for example it does not exist),
+// then it should print the following error message: "Unable to read file: my-file.txt"
+let content;
+try {
+    content = fs.readFileSync('my-file.txt', 'utf-8');
+    
+} catch(e) {
+    content = 'Unable to read file: my-file.txt';
+}
+
+console.log(content);
