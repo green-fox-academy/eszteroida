18dd6609cf82c309bea2f4c590b46789bac009e0
eszteroida <eszteroidgalaxy@gmail.com>
2019-07-11 16:48:47 +0200

    teacher-student

--- week-03/day04/teacher-student.ts
+++ week-03/day04/teacher-student.ts
@@ -1,12 +1,38 @@
 'use strict';
 
 // Create Student and Teacher classes
-// Student
-// learn() -> prints the student is learning something new
-// question(teacher) -> calls the teachers answer method
-// Teacher
-// teach(student) -> calls the students learn method
-// answer() -> prints the teacher is answering a question
+//  Student
+//      learn() -> prints the student is learning something new
+//      question(teacher) -> calls the teachers answer method
+//  Teacher
+//      teach(student) -> calls the students learn method
+//      answer() -> prints the teacher is answering a question
 // Instantiate a Student and Teacher object
 // Call the student's question() method and the teacher's teach() method
+
+class Student {
+    learn() {
+        console.log(`Student is learning something new`);
+    }
+
+    question(teacher: Teacher) {
+        teacher.answer();
+    }
+}
+
+class Teacher {
+    teach(student: Student) {
+        student.learn();
+    }
+    answer() {
+        console.log("Teacher is answering the question");
+    }
+}
+
+let me = new Student();
+let Belaba = new Teacher();
+
+me.question(Belaba);
+Belaba.teach(me);
+
 
