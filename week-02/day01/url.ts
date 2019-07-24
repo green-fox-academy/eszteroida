47280b2f054c4bce2bb5ad20eac352a2105da304
eszteroida <eszteroidgalaxy@gmail.com>
2019-07-24 19:42:21 +0200

    another solution for url.ts

--- week-02/day01/url.ts
+++ week-02/day01/url.ts
@@ -7,4 +7,7 @@ let url: string = 'https//www.reddit.com/r/nevertellmethebots';
 url = url.replace('bots', 'odds');
 url = (url.substring(0, 4) + ':' + url.slice(5));
 
+// V2
+// url = url.substring(0, 4) + ':' + url.substring(5, url.length - 4) + 'odds';
+
 console.log(url);
