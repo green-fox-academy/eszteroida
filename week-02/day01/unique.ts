7bae9e90dd9c2373ff088a60e71960c541e8743c
eszteroida <eszteroidgalaxy@gmail.com>
2019-07-24 10:26:49 +0200

    unique V2

--- week-02/day01/unique.ts
+++ week-02/day01/unique.ts
@@ -11,6 +11,15 @@ function unique(arr) {
     } return arr;
 }
 
+
+//          ??
+// function unique(arr) {
+//     arr.filter(function (element, index, self) {
+//         return index === self.indexOf(element);
+//     }) 
+// }
+
+
 //  Example
 console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]))
 //  should print: `[1, 11, 34, 52, 61]`
