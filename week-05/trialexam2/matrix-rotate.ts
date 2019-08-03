1985946e00b93db9ae4a04018f6c6ba3e70cbdb6
eszteroida <eszteroidgalaxy@gmail.com>
2019-08-03 21:19:54 +0200

    rotate matrix 8 directions

--- week-05/trialexam2/matrix-rotate.ts
+++ week-05/trialexam2/matrix-rotate.ts
@@ -1,10 +1,9 @@
 "use strict";
-// Create a function named rotateMatrix that takes an n×n integer matrix (array of arrays) as parameter
-//  and returns a matrix which elements are rotated 90 degrees clockwise.
 
-let exampleMatrix: number[][] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let exampleMatrix: number[][] = [[1, 2, 3,], [4, 5, 6], [7, 8, 9]];
+let exampleMatrix2: number[][] = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
 
-function rotateMatrix(matrix): number[][] {
+function rotateMatrix90Left(matrix): number[][] {
     let newMatrix: number[][] = [];
     for (let i: number = 0; i < matrix.length; i++) {
         let row: number[] = [];
@@ -14,4 +13,72 @@ function rotateMatrix(matrix): number[][] {
     } return newMatrix;
 }
 
-console.log(rotateMatrix(exampleMatrix));
+function rotateMatrix90Right(matrix): number[][] {
+    let newMatrix: number[][] = [];
+    for (let i: number = matrix.length - 1; i >= 0; i--) {
+        let row: number[] = [];
+        for (let j: number = 0; j < matrix.length; j++) {
+            row.push(matrix[j][i]);
+        } newMatrix.push(row);
+    } return newMatrix;
+}
+
+
+function flipMatrixHorizontal(matrix): number[][] {
+    let newMatrix: number[][] = [];
+    for (let i: number = 0; i < matrix.length; i++) {
+        let row: number[] = [];
+        for (let j: number = matrix.length - 1; j >= 0; j--) {
+            row.push(matrix[i][j]);
+        } newMatrix.push(row);
+    } return newMatrix;
+}
+
+function flipMatrixVertical(matrix): number[][] {
+    let newMatrix: number[][] = [];
+    for (let i: number = matrix.length - 1; i >= 0; i--) {
+        let row: number[] = [];
+        for (let j: number = 0; j < matrix.length; j++) {
+            row.push(matrix[i][j]);
+        } newMatrix.push(row);
+    } return newMatrix;
+}
+
+function rotateMatrix180(matrix): number[][] {
+    let newMatrix: number[][] = [];
+    for (let i: number = matrix.length - 1; i >= 0; i--) {
+        let row: number[] = [];
+        for (let j: number = matrix.length - 1; j >= 0; j--) {
+            row.push(matrix[i][j]);
+        } newMatrix.push(row);
+    } return newMatrix;
+}
+
+function flipMatrixSlash(matrix): number[][] {
+    let newMatrix: number[][] = [];
+    for (let i: number = matrix.length - 1; i >= 0; i--) {
+        let row: number[] = [];
+        for (let j: number = matrix.length - 1; j >= 0; j--) {
+            row.push(matrix[j][i]);
+        } newMatrix.push(row);
+    } return newMatrix;
+}
+
+function flipMatrixBackslash(matrix): number[][] {
+    let newMatrix: number[][] = [];
+    for (let i: number = 0; i < matrix.length; i++) {
+        let row: number[] = [];
+        for (let j: number = 0; j < matrix.length; j++) {
+            row.push(matrix[j][i]);
+        } newMatrix.push(row);
+    } return newMatrix;
+}
+
+console.log(exampleMatrix);
+console.log(rotateMatrix90Left(exampleMatrix));
+console.log(rotateMatrix90Right(exampleMatrix));
+console.log(flipMatrixHorizontal(exampleMatrix));
+console.log(flipMatrixVertical(exampleMatrix));
+console.log(rotateMatrix180(exampleMatrix));
+console.log(flipMatrixSlash(exampleMatrix));
+console.log(flipMatrixBackslash(exampleMatrix));
