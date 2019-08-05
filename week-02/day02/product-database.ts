62981b3cff5cc89e3c14ed2313bb707abc574dbe
eszteroida <eszteroidgalaxy@gmail.com>
2019-08-05 12:39:57 +0200

    week-02 day02 product database started

--- week-02/day02/product-database.ts
+++ week-02/day02/product-database.ts
@@ -1,21 +1,13 @@
 'use strict';
 
-// We are going to represent our products in a map where the keys are strings representing the product's 
-//name and the values are numbers representing the product's price.
-
-// Create a map with the following key-value pairs.
-
-// Product name (key)	Price (value)
-// Eggs	200
-// Milk	200
-// Fish	400
-// Apples	150
-// Bread	50
-// Chicken	550
-
 let products = {
-    
-}
+    Eggs: 200,
+    Milk: 200,
+    Fish: 400,
+    Apples: 150,
+    Bread: 50,
+    Chicken: 550
+};
 
 
 // Create an application which solves the following problems.
