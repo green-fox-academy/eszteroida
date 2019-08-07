9120f03ce21e111dcc0abc35bc0d824fb5a3631c
eszteroida <eszteroidgalaxy@gmail.com>
2019-08-07 12:02:37 +0200

    week-06 day03 helloworld added

---
+++ week-06/day03/helloworld/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// home page
+app.get('/', (req, res) => {
+  // render `home.ejs`
+  res.render('home', {
+    title: 'Hello World! 🌍 Hello World application is up and running. 🎉 We are done. Nice work! 🎉',
+  });
+});
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
