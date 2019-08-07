75674ca1f36a3d9bbcd9673ca535005bdf62b090
eszteroida <eszteroidgalaxy@gmail.com>
2019-08-07 15:19:23 +0200

    week-06 day03 todos added

---
+++ week-06/day03/todos/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+const todos = [
+    'get up',
+    'survive',
+    'go back to bed',
+  ];
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home', {title: 'Todos', todo: todos});
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`);
+  });
