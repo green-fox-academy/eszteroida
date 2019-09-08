15c05dde73838b36222841c414c3fd775e2a7823
eszteroida <eszteroidgalaxy@gmail.com>
2019-09-08 19:43:35 +0200

    w7d3 rest frontend project doubling and greeter endpoints added

--- week-07/day03/project/app.js
+++ week-07/day03/project/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = 8080;
-
+app.use(express.json());
 app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
@@ -11,24 +10,37 @@ app.get('/', (req, res) => {
 });
 
 app.get('/doubling', (req, res) => {
-  res.send(double());
+  let output = {};
+  if (isNaN(Number(req.query.input))) {
+    output = {
+      error: 'Please provide an input!'
+    };
+  } else {
+    output = {
+      received: req.query.input,
+      result: req.query.input * 2
+    };
+  }
+  res.send(output);
 });
-// It should be able to handle a query parameter called input, eg. http://localhost:8080/doubling?input=15
-// This endpoint should respond with a json object with the doubled amount of the input:
-// {
-//   "received": 15,
-//   "result": 30
-// }
-// If no input is provided it should return:
-// {
-//   "error": "Please provide an input!"
-// }
-// Check your application in the browser and click on the Try doubling endpoint button.
+
+app.get('/greeter', (req, res) => {
+  let message = {};
+  if (req.query.name !== undefined && req.query.title !== undefined) {
+    message = { welcome_message: `Oh, hi there ${req.query.name}, my dear ${req.query.title}!` };
+  } else if (req.query.name === undefined && req.query.title !== undefined) {
+    message = { error: "Please provide a name!" }
+  } else if (req.query.title === undefined && req.query.name !== undefined) {
+    message = { error: "Please provide a title!" }
+  } else {
+    message = { error: "Please provide a name and a title!" };
+  }
+  res.send(message);
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`The server is up and running on ${PORT}`);
 });
 
-function double(input) {
-  input *= 2;
-}
