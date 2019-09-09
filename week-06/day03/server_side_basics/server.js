087c023c42a09c303a07938450d1929fc8b7abaa
eszteroida <eszteroidgalaxy@gmail.com>
2019-09-09 14:54:31 +0200

    w6d3 server side excercise connecting css file

--- week-06/day03/server_side_basics/server.js
+++ week-06/day03/server_side_basics/server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-
 app.set('view engine', 'ejs');
-
+app.use(express.static("assets"));
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
 });
-
 app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}`);
   });
